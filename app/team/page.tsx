dd25ac4f61dd114b3a16b7bae061ad3741fb219c
Itsnatch22 <kamaumark996@gmail.com>
2025-05-08 04:31:17 -0700

    Thursday

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
-
+import Link from 'next/link';
 const teamMembers = [
   {
     id: 1,
@@ -418,8 +418,8 @@ export default function TeamPage() {
                       {/* Social Links */}
                       <div className="flex space-x-4">
                         {teamMembers[selectedMember - 1].socials.twitter && (
-                          <a
-                            href={teamMembers[selectedMember - 1].socials.twitter}
+                          <Link
+                            href={teamMembers[selectedMember - 1].socials.twitter || ''}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-blue-400 hover:text-blue-600"
@@ -428,7 +428,7 @@ export default function TeamPage() {
                             <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                             </svg>
-                          </a>
+                          </Link>
                         )}
                         {teamMembers[selectedMember - 1].socials.linkedin && (
                           <a
@@ -445,7 +445,7 @@ export default function TeamPage() {
                         )}
                         {teamMembers[selectedMember - 1].socials.github && (
                           <a
-                            href={teamMembers[selectedMember - 1].socials.github}
+                            href={teamMembers[selectedMember - 1].socials.github || ''}
                             target="_blank"
                             rel="noopener noreferrer"
                             className="text-gray-800 hover:text-gray-600"
